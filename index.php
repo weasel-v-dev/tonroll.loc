9be9bcd00ea2e5e33fed6b4d23402a1bc94555a1
Vitalii <madara.avv@gmail.com>
2023-08-16 16:59:42 +0300

    fix height

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
         </div>
         <div class="trl-bg rounded-5 py-2 px-2 d-flex  justify-content-center trl-players-bets__list overflow-auto"
              style="height: calc(100vh - 628px); min-height: 103px">
-            <div class="row g-0">
+            <div class="row g-0" style="height: fit-content">
                 <div class="col-6">
                     <div class="trl-player-bet rounded-5 px-2 py-1 d-flex align-items-center trl-bg-light-2 mx-1 my-1">
                         <div class="me-1">

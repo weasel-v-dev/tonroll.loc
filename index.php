0e81c28d1fc228ecf05c149e29ffb520ddfbbadd
Vitalii <madara.avv@gmail.com>
2023-08-16 23:25:12 +0300

    fix height

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body class="trl-bg-dark">
-<div class="container g-0">
-    <div class="px-3 pt-3">
+    <div class="container g-0">
+    <div class="px-2 pt-2">
         <div class="d-flex justify-content-between align-items-center">
             <div class="d-flex align-items-center trl-bg rounded-5 p-2">
                 <img src="/assets/img/dynamic.png" class="pe-2 trl-avatar-header-icon" alt="">
@@ -29,17 +29,17 @@
                 <div class="trl-color font-18">$ 100000.00</div>
             </div>
         </div>
-        <div class="trl-jack-pot mt-3 rounded-5 p-1 d-flex justify-content-around align-items-center">
+        <div class="trl-jack-pot mt-2 rounded-5 p-1 d-flex justify-content-around align-items-center">
             <div class="trl-jack-pot__name fw-bold">JACKPOT</div>
             <div class="trl-jack-pot__amount fw-bold">150000</div>
         </div>
-        <div class="trl-progressbar mt-3 rounded-5 position-relative">
+        <div class="trl-progressbar mt-2 rounded-5 position-relative">
             <div style="width: 10%;"
                  class="trl-progressbar__filled py-2  trl-gr-blue rounded-5 d-flex justify-content-center"></div>
             <span style="margin-top: 1px"
                   class="text-uppercase lh-1 font-14 position-absolute start-50 top-50 translate-middle">rolling in 12.35</span>
         </div>
-        <div class="trl-roll trl-bg mt-3 rounded-5">
+        <div class="trl-roll trl-bg mt-2 rounded-5">
             <div class="trl-roll__list">
                 <div class="trl-roll__el-wrap">
                     <div class="trl-roll__el trl-gr-green rounded-5"></div>
@@ -255,32 +255,28 @@
                 </div>
             </div>
         </div>
-        <div class="mt-3 trl-bg rounded-5 px-3 py-2 d-md-flex">
+        <div class="mt-2 trl-bg rounded-5 px-3 py-2 d-md-flex">
             <div class="d-flex me-md-3 justify-content-center align-items-center">
                 <div>
-                    <div class="small-btn rounded-5 py-1 px-3 fw-bold trl-bg-light-2 font-20">Min</div>
+                    <div class="small-btn rounded-5 py-1 px-3 fw-bold trl-bg-light-2 font-18">Min</div>
                 </div>
                 <div class="mx-3">
                     <input type="text" class="w-100 rounded-5 text-center font-30 text-uppercase text-white fw-bold trl-bg-light-2">
                 </div>
                 <div>
-                    <div class="small-btn rounded-5 py-1 px-3 fw-bold trl-bg-light-2 font-20">Max</div>
+                    <div class="small-btn rounded-5 py-1 px-3 fw-bold trl-bg-light-2 font-18">Max</div>
                 </div>
             </div>
             <div class="d-flex mt-2 mt-md-0 me-md-2">
-                <div class="btn-bet me-2 rounded-5 fw-bold text-center font-22 px-3 py-2 w-50 trl-gr-blue">Bet x2</div>
-                <div class="btn-bet rounded-5 fw-bold text-center font-22 px-3 py-2 w-50 trl-gr-red">Bet x2</div>
-            </div>
-            <div class="d-flex mt-2 mt-md-0">
-                <div class="btn-bet me-2 rounded-5 fw-bold text-center font-22 px-3 py-2 w-50 trl-gr-yellow">Bet x2
-                </div>
-                <div class="btn-bet rounded-5 fw-bold text-center font-22 px-3 py-2 w-50 trl-gr-green">Bet x2</div>
+                <div class="btn-bet me-2 rounded-5 fw-bold text-center font-20 px-3 py-1 w-50 trl-gr-blue">Bet x2</div>
+                <div class="btn-bet me-2 rounded-5 fw-bold text-center font-20 px-3 py-1 w-50 trl-gr-red">Bet x2</div>
+                <div class="btn-bet rounded-5 fw-bold text-center font-20 px-3 py-1 w-50 trl-gr-green">Bet x2</div>
             </div>
         </div>
-        <div class="trl-menu mt-3"></div>
+        <div class="trl-menu mt-2"></div>
     </div>
 </div>
-<div class="trl-menu trl-bg position-fixed start-0 d-flex justify-content-around align-items-center justify-content-between text-center bottom-0 w-100 px-3">
+    <div class="trl-menu trl-bg position-fixed start-0 d-flex justify-content-around align-items-center justify-content-between text-center bottom-0 w-100 px-3">
     <div class="trl-menu__item">
       <img src="/assets/img/rules.png" alt="">
       <span class="font-12 lh-0 mt-1 d-block">Rules</span>

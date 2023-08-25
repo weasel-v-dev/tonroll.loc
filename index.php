251081af517135861ae4e835a8eba52aba12b561
Vitalii <madara.avv@gmail.com>
2023-08-25 12:49:37 +0300

    fix font on micro sizes

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                 </div>
             </div>
             <div class="d-flex mt-2 justify-content-between">
-                <div class="me-2 fw-bold font-14" style="margin-bottom: -2px">Последние 100 игр:</div>
+                <div class="trl-roll__desc me-2 fw-bold font-14" style="margin-bottom: -2px">Последние 100 игр:</div>
                 <div class="d-flex">
                     <div class="trl-roll__el-history-wrap d-flex align-items-center">
                         <div class="trl-color-blue fw-bold font-14 pe-1 lh-1">10</div>
@@ -85,18 +85,18 @@
                 </div>
             </div>
         </div>
-        <ul class="nav nav-pills " id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <div class="py-2 nowrap me-3 font-14 active"  id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
-                        <span>Игроки/Ставки <span class="arr">▼</span></span>
-                    </div>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <div class="py-2 nowrap font-14"  id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">
-                        <span>История ставок <span class="arr">▼</span></span>
-                    </div>
-                </li>
-            </ul>
+        <ul class="nav nav-pills d-flex justify-content-between" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <div class="py-2 nowrap me-3 font-14 active"  id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
+                    <span>Игроки/Ставки <span class="arr">▼</span></span>
+                </div>
+            </li>
+            <li class="nav-item" role="presentation">
+                <div class="py-2 nowrap font-14"  id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">
+                    <span>История ставок <span class="arr">▼</span></span>
+                </div>
+            </li>
+        </ul>
         <div class="tab-content" id="pills-tabContent">
             <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
                 <div class="trl-bg rounded-5 py-2 px-2 d-flex  justify-content-center trl-players-bets__list overflow-auto"

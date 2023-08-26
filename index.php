04c6f8f7bfb82f617d4859638fff0507db8c835d
Vitalii <madara.avv@gmail.com>
2023-08-27 01:34:44 +0300

    fix height of menu with js

--- index.php
+++ index.php
@@ -358,22 +358,22 @@
     <script>
         const userAgent = navigator.userAgent || navigator.vendor || window.opera;
         const $menu = document.querySelector('.js-trl-menu');
-        // document.querySelectorAll('.js-trl-table').forEach(function (el) {
-        //
-        //     if (/android/i.test(userAgent)) {
-        //         el.style.height = 'calc(100vh - 500px)';
-        //         if(window.matchMedia("(max-width: 335px)").matches) {
-        //             el.style.height = 'calc(100vh - 485px)';
-        //         }
-        //     } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        //         el.style.height = 'calc(100vh - 512px)';
-        //     } else {
-        //         el.style.height = 'calc(100vh - 500px)';
-        //         if(window.matchMedia("(max-width: 335px)").matches) {
-        //             el.style.height = 'calc(100vh - 485px)';
-        //         }
-        //     }
-        // });
+        document.querySelectorAll('.js-trl-table').forEach(function (el) {
+
+            if (/android/i.test(userAgent)) {
+                el.style.height = 'calc(100vh - 518px)';
+                if(window.matchMedia("(max-width: 335px)").matches) {
+                    el.style.height = 'calc(100vh - 485px)';
+                }
+            } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+                el.style.height = 'calc(100vh - 512px)';
+            } else {
+                el.style.height = 'calc(100vh - 512px)';
+                if(window.matchMedia("(max-width: 335px)").matches) {
+                    el.style.height = 'calc(100vh - 485px)';
+                }
+            }
+        });
 
         if (/android/i.test(userAgent)) {
             $menu.style.bottom = '44px';

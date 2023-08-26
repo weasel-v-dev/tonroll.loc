5ac0f346eafae33dcf2c1e88d06d3854a34cab88
Vitalii <madara.avv@gmail.com>
2023-08-26 18:37:33 +0300

    fix height of menu

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ton Roll</title>
     <link rel="stylesheet" href="/libs/bootstrap.min.css">
@@ -307,7 +307,7 @@
         <div class="trl-menu  mt-2"></div>
     </div>
 </div>
-    <div class="position-fixed start-0 w-100 trl-menu" style="bottom: 35px">
+    <div class="position-fixed start-0 w-100 trl-menu" style="bottom: 45px">
         <div style="max-width: 540px" class="container px-2" >
             <div class=" trl-bg rounded-5 px-2  py-2">
                 <div class="d-flex justify-content-center align-items-center">
